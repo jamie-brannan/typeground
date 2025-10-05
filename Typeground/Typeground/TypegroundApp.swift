ad2d30702382b8138695f81ac3eadf1dfffce2c9
Jamie Brannan <jamiebrannan2@gmail.com>
2025-10-05 17:03:54 +0200

    Clean: Boilerplate

--- Typeground/Typeground/TypegroundApp.swift
+++ Typeground/Typeground/TypegroundApp.swift
@@ -10,12 +10,9 @@ import CoreData
 
 @main
 struct TypegroundApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
