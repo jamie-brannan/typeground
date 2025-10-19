aea7a11f51800b7d2dd80758d7dbf7f88d73d865
Jamie Brannan <jamiebrannan2@gmail.com>
2025-10-19 11:58:00 +0200

    Feat: add toolbar item usecases

---
+++ Typeground/Typeground/SettingsView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsView.swift
+//  Typeground
+//
+//  Created by Jamie Brannan on 19/10/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+  var body: some View {
+    Text("Settings")
+  }
+}
