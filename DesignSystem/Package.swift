113defc7df8f43cb6770d0f62d721dac015d057e
BRANNAN.SAPIENS <jamiebrannan2@gmail.com>
2025-11-28 13:16:28 +0100

    Feat: Add plugins to package

--- DesignSystem/Package.swift
+++ DesignSystem/Package.swift
@@ -15,11 +15,18 @@ let package = Package(
             targets: ["DesignSystem"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins.git", from: "0.62.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignSystem"
+            name: "DesignSystem",
+            plugins: [
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins"),
+                .plugin(name: "SwiftLintCommandPlugin", package: "SwiftLintPlugins")
+            ]
         ),
         .testTarget(
             name: "DesignSystemTests",
